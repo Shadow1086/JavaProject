42409336142aafd613f956509cdbcc2aebcfd078
htLiang <l15304704058@126.com>
2026-04-14 22:44:14 +0800

    ✨ feat: 初始化长空头条前后端项目结构
    - 创建 Maven 管理的 Java Web 后端模块
    - 创建 Vite 管理的 Vue 前端模块
    - 补充基础构建配置和忽略规则

---
+++ project03-ck-news/changkong-news-server/src/main/java/com/it/ck/server/pojo/HeadLinePageVo.java
@@ -0,0 +1,21 @@
+package com.it.ck.server.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ * Package: com.it.ck.server.pojo
+ * Description:
+ * <p>
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2026-2026/4/14 22:43
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+public class HeadLinePageVo implements Serializable {
+
+}
