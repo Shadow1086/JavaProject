734cbf6069d121ce4d6f4aca03bb545fe7c7fe3f
htLiang <l15304704058@126.com>
2026-04-30 15:45:07 +0800

    ✨ feat: 添加日志切面占位类

---
+++ project03-ck-news/changkong-news-springboot-server/src/main/java/com/ck/it/advice/LogAdvice.java
@@ -0,0 +1,14 @@
+package com.ck.it.advice;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Package: com.ck.it.advice
+ * Description:
+ * 
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2026-2026/4/29 23:28
+ */
+
+public class LogAdvice {
+    
+}
